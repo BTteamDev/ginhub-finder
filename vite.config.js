dad215ce892893fd05b19287707832d630de5bdb
BTteamDev <dev.btteam.pw@gmail.com>
2026-02-13 10:27:01 +1000

    added host on config

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,10 @@ export default defineConfig({
     vue(),
    // vueDevTools(),
   ],
+  preview: {
+    host: '0.0.0.0',
+    port: 4000,
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
